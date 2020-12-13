1d5eaad0af4eddf26e6089af8c21b56002abdb15
Anderson Mendes <anderson2000ano@gmail.com>
2020-12-13 17:07:33 -0300

    update logo and color

---
+++ src/components/common/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src='require("../../logo.png")' :width="width" alt="logo">
+</template>
+<script>
+export default {
+  props: ['width'],
+  name: 'Logo',
+};
+
+</script>
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home center">
     <h3 class="accent welcome">{{subname}}</h3>
-    <img src="https://raw.githubusercontent.com/AOSPK/docs/master/logo.png" alt="The Kraken Project">
+    <Logo width="350px" />
     <div class="home-desc">
-      <div class="desc rounded accent">{{description}}</div>
+    <div class="desc rounded accent">{{description}}</div>
     </div>
     <ProjectLink/>
     <Credits/>
@@ -14,12 +14,14 @@
 import ProjectLink from '../components/home/ProjectLink.vue';
 import Credits from '../components/home/Credits.vue';
 import configs from '../configs';
+import Logo from "../components/common/Logo";
 
 export default {
   name: 'Home',
   components: {
     ProjectLink,
     Credits,
+    Logo,
   },
   created() {
     document.title = 'AOSPK - The Kraken Project';
