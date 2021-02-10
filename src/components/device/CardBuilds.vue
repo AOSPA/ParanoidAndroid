203222d2bae8af16c8ab3a49cb573fd34aeaff3e
Alexandre Rangel <mamutal91@gmail.com>
2021-02-09 21:45:26 -0300

    New way to show builds

--- src/components/device/CardBuilds.vue
+++ src/components/device/CardBuilds.vue
@@ -2,15 +2,10 @@
   <div class="row center">
     <div class="col s12 m12">
       <ul class="collapsible collapsible-builds">
-        <li
-          v-for="build in deviceBuilds"
-          :key="build.id"
-          @click="setBuild(build.filename)"
-          class="buildcoll"
-        >
+        <li v-for="build in deviceBuilds" :key="build.id" @click="setBuild(build.filename)" class="buildcoll">
           <div class="collapsible-header white-text cardColor">
             <i class="material-icons">system_update</i>
-            <span style="width: 100%">{{ build.filename }}</span>
+            <span style="width: left">Android {{ build.android }} - {{ build.datetime }} ({{ build.type }})</span>
             <i class="material-icons">arrow_drop_down</i>
           </div>
 
@@ -30,7 +25,7 @@
                 <p>MD5: {{build.md5}}</p>
               </div>
               <div class="deviceprop">
-                <p>Version: Android {{build.android}}</p>
+                <p>Version: Android {{build.android}} ({{build.type}})</p>
               </div>
               <div class="deviceprop">
                 <p>Downloads: {{build.downloads}}</p>
@@ -43,68 +38,77 @@
             </div>
 
             <div class="buildbuttons">
-              <a
-                v-on:click="download(build.filename, device.codename)"
-                download
-                target="_blank"
-                class="btn"
-              >Download</a>
+              <a v-on:click="download(build.filename, device.codename)" download target="_blank" class="btn">Download</a>
             </div>
           </div>
         </li>
       </ul>
-      <Loading v-if="$store.state.buildLoader"/>
+      <Loading v-if="$store.state.buildLoader" />
     </div>
   </div>
 </template>
 <script>
-import Loading from '../common/Loading.vue';
-import { generateDownloadURL } from '../../services/sourceforge';
+  import Loading from '../common/Loading.vue';
+  import {
+    generateDownloadURL
+  } from '../../services/sourceforge';
 
-export default {
-  name: 'CardBuilds',
-  components: {
-    Loading,
-  },
-  updated() {
-    if (this.$route.params.filename) {
-      this.$store.dispatch(
-        'getIndexOfExpandedBuild',
-        this.$route.params.filename,
-      );
-      document.title = this.$route.params.filename || `Download Kraken for ${this.$route.params.codename}`;
-    }
-    this.openBuild(this.$store.state.expandedBuild);
-    this.$store.dispatch('getIndexOfExpandedBuild', '');
-  },
-  methods: {
-    setBuild(obj) {
-      const elems = document.querySelector('.collapsible-builds');
-      const instances = M.Collapsible.init(elems);
-
-      instances.options.onOpenEnd = () => this.$router.push({ name: 'filename', params: { filename: obj } });
-
-      instances.options.onCloseEnd = () => this.$router.replace({ name: 'filename', params: { filename: null } });
+  export default {
+    name: 'CardBuilds',
+    components: {
+      Loading,
     },
-    openBuild(index) {
-      if (!isNaN(index) && index !== -1) {
+    updated() {
+      if (this.$route.params.filename) {
+        this.$store.dispatch(
+          'getIndexOfExpandedBuild',
+          this.$route.params.filename,
+        );
+        document.title = this.$route.params.filename || `Download Kraken for ${this.$route.params.codename}`;
+      }
+      this.openBuild(this.$store.state.expandedBuild);
+      this.$store.dispatch('getIndexOfExpandedBuild', '');
+    },
+    methods: {
+      setBuild(obj) {
         const elems = document.querySelector('.collapsible-builds');
         const instances = M.Collapsible.init(elems);
-        instances.open(index);
-      }
+
+        instances.options.onOpenEnd = () => this.$router.push({
+          name: 'filename',
+          params: {
+            filename: obj
+          }
+        });
+
+        instances.options.onCloseEnd = () => this.$router.replace({
+          name: 'filename',
+          params: {
+            filename: null
+          }
+        });
+      },
+      openBuild(index) {
+        if (!isNaN(index) && index !== -1) {
+          const elems = document.querySelector('.collapsible-builds');
+          const instances = M.Collapsible.init(elems);
+          instances.open(index);
+        }
+      },
+      download(file, codename) {
+        M.toast({
+          html: 'Download Started'
+        });
+        location.href = generateDownloadURL(file, codename);
+      },
     },
-    download(file, codename) {
-      M.toast({ html: 'Download Started' });
-      location.href = generateDownloadURL(file, codename);
+    computed: {
+      deviceBuilds() {
+        return this.$store.state.builds;
+      },
+      device() {
+        return this.$store.state.device;
+      },
     },
-  },
-  computed: {
-    deviceBuilds() {
-      return this.$store.state.builds;
-    },
-    device() {
-      return this.$store.state.device;
-    },
-  },
-};
+  };
 </script>
