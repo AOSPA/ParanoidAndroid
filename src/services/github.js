508986aa98d3dce480880614b5d0100d791250c8
Rajkale99 <kaleraj.rk@gmail.com>
2021-04-27 00:09:51 +0530

    Update github.js

--- src/services/github.js
+++ src/services/github.js
@@ -51,7 +51,7 @@ const fetchBuilds = async (codename, variant) => {
 
 const fetchChangelog = async (codename, varient) => {
   try {
-    const res = await request(`${baseURL}/OTA/11/${codename}/changelogs_${variant}.txt', false);
+    const res = await request(`${baseURL}/OTA/11/${codename}/changelogs_${varient}.txt', false);
 
     return res.includes('404') ? 'Changelog data no found' : res;
   } catch (err) {
