a8542daacb0aeb81aa1ff541aef3bc8af2883048
rajkale99 <kaleraj.rk@gmail.com>
2021-04-26 16:24:49 +0530

    Revert "Disable theme picker for now"
    This reverts commit 09b010073c4cdfc347cf702ff1876bf20268ac0e.

---
+++ src/components/common/ThemeEngine.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a onclick="toogleMenu()" class="accent">
+      <i id="settings" class="settings material-icons pointer">color_lens</i>
+    </a>
+    <div class="menu" style="display: none">
+      <h6 class="accent">COLOR ENGINE</h6>
+      <div class="line" style="margin-bottom: 12px;"></div>
+      <div>
+        <span class="accent">BACKGROUND</span>
+        <div id="backgrounds-render"></div>
+        <div style="clear: both"></div>
+      </div>
+      <div>
+        <span class="accent">COLOR</span>
+        <div id="colors-render"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ThemeEngine',
+};
+</script>
