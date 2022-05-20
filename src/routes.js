86aff416a391cf6c719834796b2d1c096b759b27
Hernán Castañón <herna@paranoidandroid.co>
2022-05-20 08:35:36 +0200

    Adapt for GH pages
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import Changelog from "./views/Changelog.vue";
 import Team from "./views/Team.vue";
 
 export const routes = [
-  { path: "/", component: Home, name: "home" },
-  { path: "/changelog", component: Changelog, name: "changelog" },
-  { path: "/team", component: Team, name: "team" },
+  { path: "/", redirect: { name: 'home' } } ,
+  { path: "/ParanoidAndroid/", component: Home, name: "home" },
+  { path: "/ParanoidAndroid/changelog", component: Changelog, name: "changelog" },
+  { path: "/ParanoidAndroid/team", component: Team, name: "team" },
   {
-    path: "/:codename",
+    path: "/ParanoidAndroid/:codename",
     component: Device,
     name: "device",
     children: [
