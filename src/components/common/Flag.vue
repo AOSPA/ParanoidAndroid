4b8e277e0756737a43a94a64ef98212a96af6665
Jake Weinstein <jake@aospa.co>
2023-02-23 12:55:32 -0600

    Update flag URLs and add Bangladesh
    Change-Id: Ibd945ee4aefde16ceb9c0e2e56410b2ad19e789a

--- src/components/common/Flag.vue
+++ src/components/common/Flag.vue
@@ -9,45 +9,47 @@ export default {
     flag() {
       const flags = {
         AL:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-albania_1f1e6-1f1f1.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-albania_1f1e6-1f1f1.png",
         AM:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-armenia_1f1e6-1f1f2.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-armenia_1f1e6-1f1f2.png",
+        BD:
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-bangladesh_1f1e7-1f1e9.png"
         BR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-brazil_1f1e7-1f1f7.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-brazil_1f1e7-1f1f7.png",
         CH:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-switzerland_1f1e8-1f1ed.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-switzerland_1f1e8-1f1ed.png",
         CN:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-china_1f1e8-1f1f3.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-china_1f1e8-1f1f3.png",
         DE:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-germany_1f1e9-1f1ea.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-germany_1f1e9-1f1ea.png",
         ES:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-spain_1f1ea-1f1f8.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-spain_1f1ea-1f1f8.png",
         FR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/322/flag-france_1f1eb-1f1f7.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-france_1f1eb-1f1f7.png",
         ID:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-indonesia_1f1ee-1f1e9.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-indonesia_1f1ee-1f1e9.png",
         IN:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-india_1f1ee-1f1f3.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-india_1f1ee-1f1f3.png",
         KR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-south-korea_1f1f0-1f1f7.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-south-korea_1f1f0-1f1f7.png",
         KZ:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-kazakhstan_1f1f0-1f1ff.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-kazakhstan_1f1f0-1f1ff.png",
         LK:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-sri-lanka_1f1f1-1f1f0.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-sri-lanka_1f1f1-1f1f0.png",
         MU:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/348/flag-mauritius_1f1f2-1f1fa.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-mauritius_1f1f2-1f1fa.png",
         PK:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/322/flag-pakistan_1f1f5-1f1f0.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-pakistan_1f1f5-1f1f0.png",
         PT:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-portugal_1f1f5-1f1f9.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-portugal_1f1f5-1f1f9.png",
         RU:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-russia_1f1f7-1f1fa.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-russia_1f1f7-1f1fa.png",
         TR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-turkey_1f1f9-1f1f7.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-turkey_1f1f9-1f1f7.png",
         UA:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-ukraine_1f1fa-1f1e6.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-ukraine_1f1fa-1f1e6.png",
         US:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-united-states_1f1fa-1f1f8.png",
+          "https://em-content.zobj.net/thumbs/120/twitter/322/flag-united-states_1f1fa-1f1f8.png",
       };
 
       if (flags[this.country]) {
